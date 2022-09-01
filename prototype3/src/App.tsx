6e0f4ddcd859ad07c87f0d1bb93a16418a1eff21
NilsHellwig <nc.hellwig@gmail.com>
2022-09-01 17:15:15 +0200

    back to BrowserRouter

--- prototype3/src/App.tsx
+++ prototype3/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CreateSubmission, MainDashboard, Submission } from "./pages";
 
 export const App = () => {
