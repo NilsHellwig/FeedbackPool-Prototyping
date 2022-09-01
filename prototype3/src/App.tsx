b377643d6e7d340346c08bb4d11c57911491be1a
markusbink <bink-markus@web.de>
2022-09-01 16:09:34 +0200

    Remove unused import

--- prototype3/src/App.tsx
+++ prototype3/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CreateSubmission, MainDashboard, Submission } from "./pages";
 
 export const App = () => {
