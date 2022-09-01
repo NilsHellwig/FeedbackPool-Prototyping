7bba6bf29e6b778d5df626a43a568feddb13ca26
NilsHellwig <nc.hellwig@gmail.com>
2022-09-01 17:00:03 +0200

    test change paths

--- prototype3/src/App.tsx
+++ prototype3/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CreateSubmission, MainDashboard, Submission } from "./pages";
 
 export const App = () => {
@@ -6,10 +6,10 @@ export const App = () => {
     <Router>
       <>
         <Routes>
-          <Route path="/submission/create" element={<CreateSubmission />} />
-          <Route path="/submission/:submissionId" element={<Submission />} />
-          <Route path="/feedback" element={<MainDashboard />} />
-          <Route path="/" element={<MainDashboard />} />
+          <Route path="/FeedbackPool-Prototyping/submission/create" element={<CreateSubmission />} />
+          <Route path="/FeedbackPool-Prototyping/submission/:submissionId" element={<Submission />} />
+          <Route path="/FeedbackPool-Prototyping/feedback" element={<MainDashboard />} />
+          <Route path="/FeedbackPool-Prototyping" element={<MainDashboard />} />
         </Routes>
       </>
     </Router>
