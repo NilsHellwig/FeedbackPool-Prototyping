5c6c84c86fc40116030e1175c160934b38765d07
markusbink <bink-markus@web.de>
2022-09-01 14:06:45 +0200

    Remove unused files

--- prototype3/src/App.tsx
+++ prototype3/src/App.tsx
@@ -1,11 +1,7 @@
-import "./App.css";
-
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
-}
-
-export default App;
+};
